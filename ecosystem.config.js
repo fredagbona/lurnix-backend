d4a8134020cdfe79f2375d7646b1ce8908e1c6c6
fredagbona <fredagbona@gmail.com>
2025-09-02 16:26:19 +0100

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'lurnix-backend',
+      script: './dist/server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 5000
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      error_file: 'logs/error.log',
+      out_file: 'logs/out.log',
+      time: true
+    }
+  ]
+};
